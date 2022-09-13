aca6d79a47504d9313107ebb66a65be214071179
Samuel Moran <srmoran93@gmail.com>
2022-09-13 17:08:01 -0400

    spring4shell terraform demo

---
+++ spring4shell-terraform-setup/basic-terraform-setup/main.tf
@@ -0,0 +1,65 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+
+  }
+
+  required_version = ">= 1.2.0"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+
+  ami = "ami-090fa75af13c156b4"
+
+  instance_type = "t2.micro"
+
+  iam_instance_profile = "spring4shell-s3-access"
+
+  security_groups = ["spring4shell_test"]
+
+  key_name = "ec2_test_spring4shell"
+
+  tags = {
+    Name = var.instance_name
+  }
+
+
+  user_data = <<EOF
+
+  #!/bin/bash
+  
+  sudo yum update -y
+  
+  sudo yum install docker -y
+  
+  sudo yum install git -y
+  
+  sudo service docker start
+  
+  sudo usermod -a -G docker ec2-user
+  
+  sudo yum install python3
+  
+  alias python = python3
+  
+  git clone https://github.com/reznok/Spring4Shell-POC.git
+	
+	cd Spring4Shell-POC/
+	
+	sudo docker build . -t spring4shell
+	
+	docker run -d -p 80:8080 spring4shell
+  EOD
+  EOF
+
+}
--- spring4shell-terraform-setup/basic-terraform-setup/variables.tf
+++ spring4shell-terraform-setup/basic-terraform-setup/variables.tf
@@ -2,6 +2,6 @@ variable "instance_name" {
 
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "ExampleAppServerInstance"
+  default     = "spring4shell-instance"
 
 }
