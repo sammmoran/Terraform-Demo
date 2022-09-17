81eb9d9cfa7767c20e1c535d035d8a94ebddefc7
Samuel Moran <srmoran93@gmail.com>
2022-09-17 11:26:02 -0400

    gitignore update

--- c2-covenant/outputs.tf
+++ c2-covenant/outputs.tf
@@ -26,3 +26,18 @@ output "security_group_vpc" {
   value       = aws_security_group.terraform_security_group.vpc_id
 
 }
+
+
+output "iam_role_name" {
+
+  description = "Name of the IAM role attached to the instance"
+  value       = aws_iam_role.terraform_iam_role.name
+
+}
+
+output "iam_role_arn" {
+
+  description = "ARN of the IAM role attached to the instance"
+  value       = aws_iam_role.terraform_iam_role.managed_policy_arns
+
+}
