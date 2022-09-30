dd7d13eedc934e23e0a677ff9f53decf694dfc10
Samuel Moran <srmoran93@protonmail.com>
2022-09-29 20:29:57 -0400

    updated user-data with logs

--- c2-lab-setup/c2-server.sh
+++ c2-lab-setup/c2-server.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install ubuntu-desktop -y
-sudo apt install tightvncserver -y
-sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
- 
+echo "------------------ Begin preliminary Ubuntu update ---------------------- " >> logfile.txt
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf >> logfile.txt
+sudo apt-get update && sudo apt-get upgrade -y >> logfile.txt
+sudo apt-get install ubuntu-desktop -y >> logfile.txt
+sudo apt install tightvncserver -y >> logfile.txt
+echo "" >> logfile.txt
+echo "------------------- SUCCESS (1/3): TightVNC Servier Installed -------------- " >> logfile.txt
+echo "" >> logfile.txt
+sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y >> logfile.txt
 
-# Try out the following
-echo $VNC_PASSWD | vncpasswd -f > ~/.vnc/passwd
-chown -R ubuntu ~/.vnc
+
+# Try out the followinng
+echo "------------------ VNC Setup Beginning ---------------------- " >> logfile.txt
+echo $VNC_PASSWD | vncpasswd -f > ~/.vnc/passwd >> logfile.txt
+chown -R ubuntu ~/.vnc 
 chmod 0600 /home/$myuser/.vnc/passwd
 
-vncserver :1
+vncserver :1 >> logfile.txt
 
 echo '#!/bin/sh' > ~/.vnc/xstartup
 
@@ -31,22 +36,30 @@ echo 'metacity &' >> ~/.vnc/xstartup
 echo 'nautilus &' >> ~/.vnc/xstartup
 echo 'gnome-terminal &' >> ~/.vnc/xstartup
 
-vncserver -kill :1
+vncserver -kill :1 >> logfile.txt
 
-vncserver :1
+vncserver :1 >> logfile.txt
 
-git clone --recurse-submodules https://github.com/cobbr/Covenant
+echo "" >> logfile.txt
+echo "------------------ SUCCESS (2/3): VNC Setup Concluded ---------------------- " >> logfile.txt
+echo "" >> logfile.txt
+
+
+echo "------------------ Covenant Setup Initiating ---------------------- " >> logfile.txt
+git clone --recurse-submodules https://github.com/cobbr/Covenant >> logfile.txt
 
 #wget https://download.visualstudio.microsoft.com/download/pr/e137cdac-0e15-46ec-bd60-14fe6ad50c41/30c102677cc4bd0f117cc026781ec5e8/dotnet-sdk-3.1.423-linux-x64.tar.gz -y
 
-sudo snap install dotnet-sdk --classic --channel=6.0
-sudo snap alias dotnet-sdk.dotnet dotnet
-sudo snap install dotnet-runtime-60 --classic
-sudo snap alias dotnet-runtime-60.dotnet dotnet
-export DOTNET_ROOT=/snap/dotnet-sdk/current
-sudo dotnet --info
+sudo snap install dotnet-sdk --classic --channel=6.0 >> logfile.txt
+sudo snap alias dotnet-sdk.dotnet dotnet >> logfile.txt
+sudo snap install dotnet-runtime-60 --classic >> logfile.txt
+sudo snap alias dotnet-runtime-60.dotnet dotnet >> logfile.txt
+export DOTNET_ROOT=/snap/dotnet-sdk/current >> logfile.txt
+sudo dotnet --info >> logfile.txt
 
-mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.423-linux-x64.tar.gz -C $HOME/dotnet
+echo "------------------ SUCCESS (3/3): Covenant Setup Concluded ---------------------- " >> logfile.txt
+
+#mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-3.1.423-linux-x64.tar.gz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet 
 export PATH=$PATH:$HOME/dotnet
 
